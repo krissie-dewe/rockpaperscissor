e5b11e10262b747e4dad771cd080058dff05dcf3
krissie <krissie.dewe@gmail.com>
2022-05-03 17:35:08 +0700

    edit index.html and script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
 const scissor = document.querySelector('#scissor');
 const result = document.querySelector('.result');
+const player = document.querySelector('.playerScore');
+const computer = document.querySelector('.computerScore');
 
 
 rock.addEventListener('click', () => {
@@ -23,25 +25,30 @@ function playRound(playerSelection) {
 
      const computerSelection = computerPlay();
      let resultText = 'Go!';
+     let count = 0;
     
    if(playerSelection.toLowerCase() == 'rock') {
         if(computerSelection == 'rock') {
             resultText = `You Draw, ${playerSelection} vs ${computerSelection}`;
             console.log(resultText);
+            count = 2;
         } else if(computerSelection == 'paper')  {
             resultText = `You Loose, ${playerSelection} vs ${computerSelection}`;
             console.log(resultText);
         } else if(computerSelection == 'scissor') {
             resultText = `You WIN, ${playerSelection} vs ${computerSelection}`;
             console.log(resultText);
+            count = 1;
         }
     }else if(playerSelection.toLowerCase() == 'paper') {
         if(computerSelection == 'rock') {
             resultText = `You WIN, ${playerSelection} vs ${computerSelection}`;
             console.log(resultText);
+            count = 1;
         } else if(computerSelection == 'paper')  {
             resultText = `You Draw, ${playerSelection} vs ${computerSelection}`;
             console.log(resultText);
+            count = 2;
         } else if(computerSelection == 'scissor') {
             resultText = `You Loose, ${playerSelection} vs ${computerSelection}`;
             console.log(resultText);
@@ -53,15 +60,43 @@ function playRound(playerSelection) {
         } else if(computerSelection == 'paper')  {
             resultText = `You WIN, ${playerSelection} vs ${computerSelection}`;
             console.log(resultText);
+            count = 1;
         } else if(computerSelection == 'scissor') {
             resultText = `You Draw, ${playerSelection} vs ${computerSelection}`;
             console.log(resultText);
+            count = 2;
         }
     } else {
         resultText = 'wrong input';
         console.log(resultText);
     }
     result.textContent = resultText;
+
+    if(count == 1) {
+        let plScore = parseInt(player.textContent) + 1;
+
+        player.textContent = plScore;
+
+
+        if(parseInt(player.textContent) == 5) {
+            result.textContent = "YOU WIN !!!";
+            player.textContent = 0;
+            computer.textContent = 0;
+    
+        }
+
+    } else if(count == 0) {
+        let compScore = parseInt(computer.textContent) + 1;
+        computer.textContent = compScore;
+
+        if(parseInt(computer.textContent) == 5) {
+            result.textContent = "YOU LOOSE";
+            player.textContent = 0;
+            computer.textContent = 0;
+    
+        }
+    }
+
 }
 
 function computerPlay() {
